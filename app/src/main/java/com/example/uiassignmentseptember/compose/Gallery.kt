d2db484e920198d481aac53d2cd7a272508b5ca1
Peter <rawiwatpan@gmail.com>
2023-09-27 12:28:56 +0700

    commit 29

--- app/src/main/java/com/example/uiassignmentseptember/compose/Gallery.kt
+++ app/src/main/java/com/example/uiassignmentseptember/compose/Gallery.kt
@@ -1,6 +1,7 @@
 package com.example.uiassignmentseptember.compose
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,16 +9,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.SubcomposeAsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.uiassignmentseptember.getImageIds
 
 @Composable
@@ -31,7 +39,7 @@ fun Gallery(
             .fillMaxSize()
     ) {
         LazyVerticalGrid(
-            columns = GridCells.Fixed(3)
+            columns = GridCells.Fixed(2)
         ) {
             items(
                 images,
@@ -53,15 +61,24 @@ fun PhotoInGallery(
             .size(size.dp)
             .padding(5.dp)
     ) {
-        SubcomposeAsyncImage(
-            model = imageId,
-            contentDescription = null,
-            loading = {
-                CircularProgressIndicator()
-            },
-            modifier = Modifier
-                .fillMaxSize(),
-            contentScale = ContentScale.FillBounds
+        val painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current).data(data = imageId).apply(block = fun ImageRequest.Builder.() {
+                crossfade(500)
+            }).build()
         )
+
+        val painterState = painter.state
+
+        if (painterState == AsyncImagePainter.State.Loading(painter)) {
+            CircularProgressIndicator()
+        } else {
+            Image(
+                painter = painter,
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                contentDescription = ""
+            )
+        }
     }
 }
